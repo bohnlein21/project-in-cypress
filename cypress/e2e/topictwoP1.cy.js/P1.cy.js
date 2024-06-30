c7f0a55ab2342c6203f43b62fb38d7c8529fd072
bohnlein21 <matthewsbohnleinofficial@gmail.com>
2024-06-30 15:58:11 -0500

    this is a test spec on the alert buttons on the webiste tools qa

---
+++ cypress/e2e/topictwoP1.cy.js/P1.cy.js
@@ -0,0 +1,61 @@
+import 'cypress-iframe'
+
+
+function website() {
+    cy.visit('https://demoqa.com/alerts');
+}
+
+
+describe('login Test', () => {
+    it ('passes', () => {
+ website();
+ //click on alert button
+ cy.get('#alertButton').click();
+//firing window alert
+ cy.on('window:alert',(txt) => {
+//mocha assertion
+    expect(txt).to.contain('You clicked a button');
+
+        })
+    })
+}) 
+
+describe ('timer alert', () => {
+    
+    it ('passes', () => {
+        website();
+
+        cy.get('#timerAlertButton').click();
+
+        cy.on('window:alert',(txt) => {
+            expect(txt).to.contain('This alert appeared after 5 seconds');
+        })
+    })
+})
+
+describe ('timer alert', () => {
+    it ('passes', () => {
+        website();
+
+        cy.get('#confirmButton').click();
+
+        cy.on('window:alert',(txt) => {
+            expect(txt).to.conatin('Do you confirm action?');
+        })
+    })
+})
+
+describe('single frame test', function () {
+    
+    it('passes',function () {
+
+        website();
+
+        cy.frameLoaded('#frame1');
+
+        cy.iframe().find()
+
+
+
+    })
+})
