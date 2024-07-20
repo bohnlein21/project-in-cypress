b11efd9f31cb68bab60b522ca3d3986f91934d79
bohnlein21 <matthewsbohnleinofficial@gmail.com>
2024-07-20 14:30:25 -0500

    update to the command to the command.js file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login',(username, pw) => {
+    cy.visit('https://www.saucedemo.com')
+    cy.get('[data-test="username"]').type(username)
+    cy.get('[data-test="password"]').type(pw)
+    cy.get('[data-test="login-button"]').click()
+});
+
